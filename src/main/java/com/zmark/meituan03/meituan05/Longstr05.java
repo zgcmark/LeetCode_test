d49138454f6ca88fe068ffb2300cc66887af965f
郑光宸 <zhengguangchen@huohua.cn>
2020-05-27 21:12:04 +0800

    股票问题 309 题

---
+++ src/main/java/com/zmark/meituan03/meituan05/Longstr05.java
@@ -0,0 +1,26 @@
+package com.zmark.meituan03.meituan05;
+
+/**
+ * @author zhengguangchen
+ */
+//给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+//
+//        示例 1：
+//
+//        输入: "babad"
+//        输出: "bab"
+//        注意: "aba" 也是一个有效答案。
+//        示例 2：
+//
+//        输入: "cbbd"
+//        输出: "bb"
+
+public class Longstr05 {
+    public String longestPalindrome(String s) {
+        //定义状态  dp[i][j]=true  ,true 是 回文, false 不是
+
+        //定义状态转移方程
+//        dp[i][j]=true
+
+    }
+}
